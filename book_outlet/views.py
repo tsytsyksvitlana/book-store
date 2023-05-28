154542ce0143e2f25d644b05f261702fa8bddcad
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-05-28 23:07:39 +0300

    Rendered the Details Page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 
@@ -8,4 +9,14 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    book=get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "isbestseller": book.isbestselling
     })
