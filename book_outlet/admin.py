60891500bdcb4f89389fd8c85889cc00f8b4029a
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-05-30 16:38:43 +0300

    admin section ended

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -5,5 +5,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields={"slug": ("title", )}
+    list_filter= ("author", "rating", )
+    list_display=("title", "author", )
 
 admin.site.register(Book, BookAdmin)
